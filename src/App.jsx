d80539e51cc0f62d144fe204ceed3d8c39e3d2ac
mustafajawed <muhammadmustafamuhamamdjawedkh@gmail.com>
2024-09-21 11:32:34 +0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Tictactoe from './Tictactoe'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1><Tictactoe /></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Tictactoe.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Tictactoe = () => {
+    return (
+        <>
+            <div className='container text-center'><h3 className='fst-italic d-flex justify-content-center fs-8px align-items-center' >Tic Tac Toe</h3></div></>
+    )
+}
+
+export default Tictactoe
